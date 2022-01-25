f9600ef89836996a686e402a083416ca64390dec
han253 <han315730@gmail.com>
2022-01-25 07:20:42 -0500

    add models

--- libreria/libreria/urls.py
+++ libreria/libreria/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('libros/', include('libros.urls')),
+    path('', include('libros.urls')),
     path('admin/', admin.site.urls),
 ]
--- libreria/libros/models.py
+++ libreria/libros/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    birth_date = models.DateField()
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    year = models.IntegerField()
+    cost = models.IntegerField()
+    author =  models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
