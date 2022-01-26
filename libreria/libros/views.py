ca687487aa42babbebee7f18a16c25ea81c28f57
han253 <han315730@gmail.com>
2022-01-26 06:58:41 -0500

    add query and view

--- libreria/libros/views.py
+++ libreria/libros/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Author
+
 # Create your views here.
 def index(request):
     return HttpResponse("HOLA MUNDO.")
+
+def listarAutores(request):
+    lista = Author.objects.all()
+    output = ', '.join([a.first_name for a in lista])
+    return HttpResponse(output)
